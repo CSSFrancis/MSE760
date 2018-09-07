22fa5d0e648765e32255314505a344defe27926a
CSFrancis <csfrancis@wisc.edu>
2018-09-06 20:56:37 -0500

    Initial Commit

---
+++ MSE760/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MSE760
+//
+//  Created by Carter Francis on 9/6/18.
+//  Copyright © 2018 Carter Francis. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
