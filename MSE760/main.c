a0456232376c4c4ed090b6d55a50c4a261af69d0
CSFrancis <csfrancis@wisc.edu>
2018-09-07 09:24:06 -0500

    Version 0.1- Turning initializing unit cell into a function

--- MSE760/main.c
+++ MSE760/main.c
@@ -5,11 +5,40 @@
 //  Created by Carter Francis on 9/6/18.
 //  Copyright © 2018 Carter Francis. All rights reserved.
 //
-
+#include <math.h>
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
+int main() {
     // insert code here...
-    printf("Hello, World!\n");
+    double uCell = .541;
+    int nCells = 5;
+    double aPos;
+    aPos = createFCC(nCells, uCell);
+    
+
+    // basis
+    
     return 0;
 }
+
+//functions
+double createFCC (int numCells,double unitCell){
+    int atomsPerCell =4;
+    int numAtoms = (pow(numCells,3)*atomsPerCell);
+    double atomPositions[3][500];
+    static int count = 0;
+    for (int i=0; i < numCells; i++) {
+        for (int j = 0; j<numCells;j++){
+            for (int k=0; k < numCells; k++) {
+                atomPositions[0][count] = i*unitCell;
+                atomPositions[1][count] = j*unitCell;
+                atomPositions[2][count] = k*unitCell;
+                atomPositions[0][count+1] = i*unitCell+ (unitCell/2);
+                atomPositions[1][count+1] = j*unitCell+ (unitCell/2);
+                atomPositions[2][count+1] = k*unitCell;
+                count =count +2;
+            }
+        }
+    }
+    return **atomPositions;
+    }
