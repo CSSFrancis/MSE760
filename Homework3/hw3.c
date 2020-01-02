c7883f390edb7f210f762ec69908df7878896b99
Carter Francis <csfrancis@wisc.edu>
2020-01-02 09:33:57 -0600

    Changed somethings with regard to the density. Might still need some work

--- Homework3/hw3.c
+++ Homework3/hw3.c
@@ -12,21 +12,21 @@
 int main() {
     //initializing variables
     srand (time(NULL));
-    double cutoff= 2.0;
-    int a=4; //num of unit cells
+    double cutoff= 5.0;
+    int a = 4; //num of unit cells
     double sigma = 3.4;
-    double uCell = 5.7/sigma;// in angstroms
+    double uCell = 6.0/sigma; // in angstroms, for a density of .84?
     int nCells = a+1;
     double epsilon = 0.0104;
     int nAtoms = nCells*nCells*nCells*4;
     double X[nAtoms]; double Y[nAtoms]; double Z[nAtoms]; double E[nAtoms];
-    int timesteps =500000;
-    double stepSize =.1;
-    double T =2.0;
-    double Length = uCell*nCells/2;
-    //initializing simulation
+    int timesteps = 100000;
+    double stepSize = 0.1;
+    double T = 2.0;
+    double Length = uCell * nCells / 2;
+    // Initializing simulation
     createFCC(nCells, uCell, X, Y, Z);
-    double en [nAtoms-1];
+    double en [nAtoms - 1];
     // Calculate the energy of every single atom.
     calculate_all_atom_energy(X, Y, Z, en, nAtoms,Length, cutoff);
     FILE *out_energy_file = fopen("../Homework3/out/energies.txt", "w");
@@ -34,7 +34,7 @@ int main() {
     for (int l=0; l<timesteps; l++)
     {
         numChanged += monteCarloStep(X, Y, Z, T, nAtoms, stepSize,Length, en, cutoff);
-        if (l%1000==0){
+        if (l%50==0){
             double energySum = calculate_all_atom_energy(X, Y, Z, en ,nAtoms,Length, cutoff);
             fprintf(out_energy_file, "%g\n", energySum);
         }
--- Homework3/hw3.h
+++ Homework3/hw3.h
@@ -1,13 +1,13 @@
-//
-//  homework2.h
+//  Auxilliary functions for Homework #3.  MSE 760.
+//  hw3.h
 //  MSE760
 //
 //  Created by Carter Francis on 11/1/18.
 //  Copyright © 2018 Carter Francis. All rights reserved.
 //
 
-#ifndef homework2_h
-#define homework2_h
+#ifndef hw3_h
+#define hw3_h
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -42,47 +42,13 @@ int createFCC (int numCells, double UnitCell, double X[], double Y[], double Z[]
     return 0;
 }
 
-//Calculating Potential
-double calculate_Potential(double X[],double Y[], double Z[], double energy[],int numAtoms, double L)
-{
-    //double potential;
-    double dist;
-    double energySum =0.0;
-    //double cutoff = .2;
-    for (int i=0; i <numAtoms-1; i=i+1){
-        //determining atoms within some cutoff
-        double energyHolder =0.0;
-        for(int j=i+1; j <numAtoms; j=j+1){
-            double difX =(X[i] - X[j]);
-            double difY =(Y[i] - Y[j]);
-            double difZ =(Z[i] - Z[j]);
-            //For periodic boundry conditions
-            if (difX > L){difX = difX-2.0*L;}
-            if (difX <= -L){difX = difX+2.0*L;}
-            if (difY > L){difY = difY-2.0*L;}
-            if (difY <= -L){difY = difY+2.0*L;}
-            if (difZ > L){difZ = difZ-2.0*L;}
-            if (difZ <= -L){difZ = difZ+2.0*L;}
-            //calculating the distance from atom i to j
-            dist = sqrt(pow(difX,2.0) + pow(difY,2.0) + pow(difZ,2.0));
-            //printf("%g \n", dist);
-            // calculating the forces on the particles
-            energyHolder = energyHolder + 4.0 * (pow((1/dist),12) - pow((1/dist),6));
-        }
-        energySum = energySum +energyHolder;
-        energy[i] = energyHolder;
-        
-    }
-    return energySum;
-}
-
-// Calculating the energy of a single atom. Still needs a reasonable cutoff to speed up the calculations...
+// Calculating the energy of a single atom. Uses some cutoff for calculating the energy
 double calculateAtomEnergy(double X[],double Y[], double Z[], int rAtom, double Xrand, double Yrand,double Zrand,
         int natom, double Len, double cutoff)
 {
-    double e = 0.0;
-    double d;
-    for (int i=0; (i<natom && i !=rAtom); i++)
+    double e = 0.0; // Initializing the energu
+    double d; // initializing the distance
+    for (int i=0; (i<natom && i !=rAtom); i++) // Compare atom with every other atom.
     {
         // Distance from atom i to the a random atom
         double dX =(X[i] - Xrand); double dY =(Y[i] - Yrand); double dZ =(Z[i] - Zrand);
@@ -93,25 +59,28 @@ double calculateAtomEnergy(double X[],double Y[], double Z[], int rAtom, double 
         if (dY <= -Len){dY = dY+2.0*Len;}
         if (dZ > Len){dZ = dZ-2.0*Len;}
         if (dZ <= -Len){dZ = dZ+2.0*Len;}
-        d = sqrt(pow(dX, 2.0) + pow(dY,2.0) + pow(dZ,2.0));
-        if (d < cutoff){e = e+ 4.0 * (pow((1/d),12) - pow((1/d),6));}
+        d = sqrt(pow(dX, 2.0) + pow(dY,2.0) + pow(dZ ,2.0)); // Calculating the distance
+        if (d < cutoff){e = e+ 4.0 * (pow((1/d),12) - pow((1/d),6));} // Only calculate potential energy if inside some cutoff
     }
     return e;
 }
 
-// Calculating the energy of a single atom. Still needs a reasonable cutoff to speed up the calculations...
+// Calculating the energy of a every atom. Still needs a reasonable cutoff to speed up the calculations...
 double calculate_all_atom_energy(double X[],double Y[], double Z[], double energy[], int numAtoms, double L, double cutoff)
 {
     double dist;
     double energySum =0.0;
+    // Need to do everything the long way just to make sure that the energies for each atom are calculated correctly.
     for (int i=0; i <numAtoms-1; i=i+1){
         //determining atoms within some cutoff
         double energyHolder =0.0;
-        for(int j=i+1; j <numAtoms; j=j+1){
+        for(int j=0; j <numAtoms && j!=i; j=j+1){
+            // Calculating the distance from atom i to atom j
             double difX =(X[i] - X[j]);
             double difY =(Y[i] - Y[j]);
             double difZ =(Z[i] - Z[j]);
-            //For periodic boundry conditions
+            
+            // For periodic boundary conditions
             if (difX > L){difX = difX-2.0*L;}
             if (difX <= -L){difX = difX+2.0*L;}
             if (difY > L){difY = difY-2.0*L;}
@@ -122,7 +91,7 @@ double calculate_all_atom_energy(double X[],double Y[], double Z[], double energ
             dist = sqrt(pow(difX,2.0) + pow(difY,2.0) + pow(difZ,2.0));
             if (dist < cutoff) {energyHolder = energyHolder + 4.0 * (pow((1/dist),12) - pow((1/dist),6));}
         }
-        energySum = energySum +energyHolder;
+        energySum = energySum + energyHolder;
         energy[i] = energyHolder;
 
     }
@@ -134,27 +103,32 @@ double calculate_all_atom_energy(double X[],double Y[], double Z[], double energ
 
 double monteCarloStep(double X[],double Y[], double Z[], double T, int natoms, double stepSize, double L, double energy[],double cutoff)
 {
-    double kb=0.000086173;
-    //pick a random atom from all of the atoms
+    // Pick a random atom from all of the atoms
     int randomAtom = (rand() % natoms);
     // Old positions
     double rmax = RAND_MAX;
-    // delta change of x,y,z
+    // Delta change of x,y,z
     double dx = rand()/ rmax -0.5; double dy = rand()/rmax -0.5; double dz = rand()/rmax-0.5;
     // New positions
     double newX = X[randomAtom] + dx * stepSize;
     double newY = Y[randomAtom] + dy * stepSize;
     double newZ = Z[randomAtom] + dz * stepSize;
+    // Periodic boundary conditions
+    if (newX > L){newX = newX-2.0*L;}
+    if (newX <= -L){newX = newX+2.0*L;}
+    if (newY > L){newY = newY-2.0*L;}
+    if (newY <= -L){newY = newY + 2.0 * L;}
+    if (newZ > L){newZ = newZ - 2.0 * L;}
+    if (newZ <= -L){newZ = newZ + 2.0 * L;}
     // New energy.  Just calculating the energy for the one atom that changed position.
     double newE = calculateAtomEnergy(X, Y, Z, randomAtom, newX, newY, newZ, natoms, L,cutoff);
-    double prob = exp(-(newE-energy[randomAtom])/(kb*T));
+    double prob = exp(-(newE-energy[randomAtom])/T);
     if (prob> 1 || prob > rand()/rmax)
     {
         energy[randomAtom] = newE;
         X[randomAtom]=newX;Y[randomAtom]=newY;Z[randomAtom]=newZ;
-
     }
-    return (prob> 1 || prob >rand()/rmax);
+    return (prob > 1 || prob >rand()/rmax);
 }
 
 
